1df2d61f69c46ae9a4bc0b3c3f21db77905a35b9
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-11-24 09:42:52 -0600

    completed fizz

---
+++ fizz_buzz.php
@@ -0,0 +1,40 @@
+<?php
+
+// Write a program that prints the numbers from 1 to 100. 
+// But for multiples of three print “Fizz” instead of the number 
+// and for the multiples of five print “Buzz”. 
+// For numbers which are multiples of both three 
+// and five print “FizzBuzz”."
+
+
+$num1 = 1;
+while ($num1 <= 100) {
+	// Only print $num1 if $num1 is NOT divisible by 3 OR is
+	// NOT divisible by 5
+	if (($num1 % 3 != 0) && ($num1 % 5 != 0)) {
+		echo $num1 . PHP_EOL;
+	} if ($num1 % 3 == 0) {
+		echo "Fizz" . PHP_EOL;
+	} if ($num1 % 5 == 0) {
+		echo "Buzz" . PHP_EOL;
+	} if ($num1 % 3 == 0 && $num1 % 5 == 0) {
+		echo "FizzBuzz" . "\n";
+	}
+$num1++;
+}
+
+$num1 = 1;
+while ($num1 <= 100) {
+	// Only print $num1 if $num1 is NOT divisible by 3 OR is
+	// NOT divisible by 5
+	if (($num1 % 3 != 0) && ($num1 % 5 != 0)) {
+		echo $num1 . PHP_EOL;
+	} if ($num1 % 3 == 0) {
+		echo "Fizz" . PHP_EOL;
+	} if ($num1 % 5 == 0) {
+		echo "Buzz" . PHP_EOL;
+	} if ($num1 % 3 == 0 && $num1 % 5 == 0) {
+		echo "FizzBuzz" . "\n";
+	}
+$num1++;
+}

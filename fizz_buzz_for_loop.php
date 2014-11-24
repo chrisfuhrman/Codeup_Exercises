94e391bf4e1a0bc63f4cc471dc8e402963c1cb64
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-11-24 17:34:29 -0600

    completed fizzbuzz w-for loop

---
+++ fizz_buzz_for_loop.php
@@ -0,0 +1,25 @@
+<?php
+
+// Write a program that prints the numbers from 1 to 100.
+//  But for multiples of three print “Fizz” instead of the number
+//   and for the multiples of five print “Buzz”. 
+//   For numbers which are multiples of both three and five print
+//    “FizzBuzz”.
+
+for ($i = 1; $i <= 100; $i++) {
+
+	if (($i % 5 == 0) && ($i % 3 ==0)) {
+		echo "FizzBuzz" . PHP_EOL; 
+	}
+	elseif ($i % 3 == 0) {
+		echo "Fizz" . PHP_EOL;
+	}
+
+	elseif ($i % 5 == 0) {
+		echo "Buzz" . PHP_EOL;
+	}
+
+	else {
+		echo "$i" . PHP_EOL;
+	}
+}

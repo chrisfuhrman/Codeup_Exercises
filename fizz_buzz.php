56d6c95856d9bac2d7ff60f66878263e856f65b3
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-11-25 10:55:41 -0600

    cleaner code

--- fizz_buzz.php
+++ fizz_buzz.php
@@ -6,20 +6,18 @@
 // For numbers which are multiples of both three 
 // and five print “FizzBuzz”."
 
-
 $num1 = 1;
 while ($num1 <= 100) {
 	// Only print $num1 if $num1 is NOT divisible by 3 OR is
 	// NOT divisible by 5
-	if (($num1 % 3 != 0) && ($num1 % 5 != 0)) {
-		echo $num1 . PHP_EOL;
-	} if ($num1 % 3 == 0) {
-		echo "Fizz" . PHP_EOL;
-	} if ($num1 % 5 == 0) {
-		echo "Buzz" . PHP_EOL;
-	} if ($num1 % 3 == 0 && $num1 % 5 == 0) {
+	if (($num1 % 3 == 0) && ($num1 % 5 == 0)) {
 		echo "FizzBuzz" . PHP_EOL;
+	} elseif ($num1 % 3 == 0) {
+		echo "Fizz" . PHP_EOL;
+	} elseif ($num1 % 5 == 0) {
+		echo "Buzz" . PHP_EOL;
+	} else {
+		echo "$num1" . PHP_EOL;
 	}
 $num1++;
 }
-

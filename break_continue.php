81aee5031015fd216ac67ca5ebde0b51f8e6d6b2
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-11-25 15:43:26 -0600

    complete

---
+++ break_continue.php
@@ -0,0 +1,19 @@
+<?PHP
+
+for ($i = 1; $i <= 100 ; $i++) { 
+	
+	if ($i % 2 != 0) {
+	continue;
+	}
+	echo $i . PHP_EOL;
+}
+
+
+
+
+for ($i = 1; $i <= 100 ; $i++) { 
+		echo $i . PHP_EOL;
+		if ($i > 9) {
+			break;
+		}	
+}

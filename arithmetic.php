c7d635d9cdbd6239f7d8f2b5f2b852c84ad07985
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-11-26 13:38:19 -0600

    updated w-modulus

--- arithmetic.php
+++ arithmetic.php
@@ -10,16 +10,22 @@ function subtract($a, $b) {
 	echo $a - $b . PHP_EOL;
 }
 
-subtract(5,5);
+subtract(5, 5);
 
 function multiply($a, $b) {
 	echo $a * $b . PHP_EOL;
 }
 
-multiply(5,5);
+multiply(5, 5);
 
 function divide($a, $b) {
 	echo $a / $b . PHP_EOL;
 }
 
-divide(5,5);
+divide(5, 5);
+
+function modulus($a, $b) {
+	echo $a % $b . PHP_EOL;
+}
+
+modulus(5, 5);

7d24a03dc69f9eeb83d98f3deab3c58e19c4904e
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-11-26 16:28:34 -0600

    using return

--- arithmetic.php
+++ arithmetic.php
@@ -5,38 +5,44 @@ function errorMessage($a, $b) {
 	echo "ERROR!!!!! Both arguments must be numbers.  You entered \"$a\" & \"$b\".\n";
 }
 
+function validateInput($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+		return true;
+	}
+}
+
 // Addition function
 function add($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a + $b . PHP_EOL;
+	if (validateInput($a, $b)) {
+		return $a + $b . PHP_EOL;
     } else {
 		errorMessage($a, $b);
 	}
 }
 
-add(five, 5);
+add(10, 5);
 
 // Subtration function
 function subtract($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a - $b . PHP_EOL;
+	if (validateInput($a, $b)) {
+		return $a - $b . PHP_EOL;
     } else {
 		errorMessage($a, $b);
     }
 }
 
-subtract(a, 5);
+subtract(10, 5);
 
 // Multiply function
 function multiply($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a * $b . PHP_EOL;
+	if (validateInput($a, $b)) {
+		return $a * $b . PHP_EOL;
     } else {
 		errorMessage($a, $b);
 	}
 }
 
-multiply(5, a);
+multiply(5, 5);
 
 // Function that creates an error msg if user trys to divide by ZERO.
 function errorMessageDivisibleByZero($b) {
@@ -45,26 +51,28 @@ function errorMessageDivisibleByZero($b) {
 
 // Divisible function
 function divide($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (validateInput($a, $b)) {
 		if ($b != 0) {
-			echo $a / $b . PHP_EOL;
+			return $a / $b . PHP_EOL;
 		} else {
 			errorMessageDivisibleByZero ($b);
+			return false;
 		}
 	} else {
 		errorMessage($a, $b);
 	}
 }
 
-divide(500, 0);
+divide(500, 5);
 
 // Modulus function
 function modulus($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
+	if (validateInput($a, $b)) {
 		if ($b != 0) {
-			echo $a % $b . PHP_EOL;
+			return $a % $b . PHP_EOL;
 		} else {
 			errorMessageDivisibleByZero ($b);
+			return false;
 		}
 	} else {
 	errorMessage($a, $b);

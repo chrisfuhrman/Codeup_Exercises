e80b49e61d931d474346bffb3fcdae637c36ffa1
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-11-26 14:55:54 -0600

    updated

--- arithmetic.php
+++ arithmetic.php
@@ -1,31 +1,58 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a + $b . PHP_EOL;
+    } else {
+        echo "ERROR!!!!! Both arguments must be numbers.  You entered \"$a\" & \"$b\".\n";
+    }
 }
 
-add(5, 5);
+add(five, 5);
 
 function subtract($a, $b) {
-	echo $a - $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a - $b . PHP_EOL;
+    } else {
+        echo "ERROR!!!!! Both arguments must be numbers. You entered \"$a\" & \"$b\".\n";
+    }
 }
 
-subtract(5, 5);
+subtract(a, 5);
 
 function multiply($a, $b) {
-	echo $a * $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a * $b . PHP_EOL;
+    } else {
+        echo "ERROR!!!!! Both arguments must be numbers. You entered \"$a\" & \"$b\".\n";
+    }
 }
 
-multiply(5, 5);
+multiply(5, a);
 
 function divide($a, $b) {
-	echo $a / $b . PHP_EOL;
+	if (is_numeric($a) && is_numeric($b)) {
+		if ($b != 0) {
+			echo $a / $b . PHP_EOL;
+		} else {
+			echo "ERROR! You entered $b for your 2nd input. You cannot divide by ZERO!!\n";
+		}
+	} else {
+        echo "ERROR!!!!! Both arguments must be numbers. You entered \"$a\" & \"$b\".\n";
+    }
 }
 
-divide(5, 5);
+divide(500, a);
 
-function modulus($a, $b) {
-	echo $a % $b . PHP_EOL;
-}
+// function modulus($a, $b) {
+// 	if (is_numeric($a) && is_numeric($b)) {
+// 		echo $a % $b . PHP_EOL;
+//     } else {
+//         echo "ERROR!!!!! Both arguments must be numbers\n";
+//     }
+// }
 
-modulus(5, 5);
+
+// modulus(5, 5);
+
+

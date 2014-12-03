ad4df523f30eedeb2590ba1b5fcd58e39d8e8f6b
Chris Fuhrman <chrisfuhrman@gmail.com>
2014-12-03 11:13:46 -0600

    completed

--- list_with_commas.php
+++ list_with_commas.php
@@ -2,8 +2,22 @@
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 
+
+
 function humanizedList($physicistsString) {
+	
 	$array = explode(', ', $physicistsString);
+
+	echo 'sort alphabetically? (Y) or (N)';
+	$sort = trim(fgets(STDIN));
+
+	//sort array by first name, if user wants it.
+	if ($sort == 'Y') {
+		asort($array);
+	} else {
+
+	}
+	//pop off last person in order to add 'and' in the string (to add back later)
 	$addName = array_pop($array);
 
 	$famousFakePhysicists = implode(', ', $array);
